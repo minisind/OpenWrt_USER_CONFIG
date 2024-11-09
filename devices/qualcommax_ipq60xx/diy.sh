615f75e24be0a4393f02aa83b385549baf2747b1
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-09 16:48:06 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,4 +4,6 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
+git clone https://github.com/qosmio/nss-packages.git package/nss-packages -b NSS-12.5-K6.x
+git clone https://github.com/qosmio/sqm-scripts-nss.git package/sqm-scripts-nss
 
