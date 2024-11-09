44d932628422c5e5b03b98bcfc1a47df3f7f3489
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-09 21:20:59 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,9 +4,8 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf target/linux/generic package/feeds/kiddin9/shortcut-fe package/feeds/kiddin9/simulated-driver package/feeds/kiddin9/fast-classifier package/feeds/kiddin9/*_QMI_WWAN
-git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x target/linux/generic
+rm -rf package/firmware package/boot/uboot-envtools target/linux/qualcommax
 
-git clone https://github.com/qosmio/nss-packages.git package/nss-packages -b NSS-12.5-K6.x
-git clone https://github.com/qosmio/sqm-scripts-nss.git package/sqm-scripts-nss
+git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/firmware package/boot/uboot-envtools target/linux/qualcommax
 
+rm -rf target/linux/qualcommax/patches-6.6/06*-qca-*.patch
