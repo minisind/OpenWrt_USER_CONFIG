2ac03e7dc995f9d9d88c2c593357c08fd54ac709
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-09 21:46:13 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,8 +4,10 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/firmware package/boot/uboot-envtools target/linux/qualcommax/!(Makefile)
+rm -rf package/firmware package/boot/uboot-envtools target/linux/qualcommax/ipq60xx/!(target.mk)
 
 git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x package/firmware package/boot/uboot-envtools target/linux/qualcommax
+
+git_clone_path openwrt-24.10 https://github.com/LiBwrt-op/openwrt-6.x mv target/linux/qualcommax/ipq60xx
 
 rm -rf target/linux/qualcommax/patches-6.6/06*-qca-*.patch
