fdd931474701562732cc1c23cf44e7c974da0969
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-13 06:00:11 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -8,6 +8,7 @@ rm -rf target/linux/qualcommax package/boot/uboot-envtools package/firmware pack
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware package/kernel/mac80211 package/qca package/boot/uboot-envtools
 
 sed -i "s/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/" target/linux/qualcommax/Makefile
+sed -i -e 's,wpad-openssl,wpad-basic-mbedtls,g' target/linux/qualcommax/Makefile
 
 
 
