1a50d5883a80720e615bb9fd749d6aa061a45fe2
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-13 06:10:02 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -4,8 +4,8 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf target/linux/qualcommax package/boot/uboot-envtools package/firmware
-git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware package/kernel/mac80211 package/boot/uboot-envtools
+rm -rf target/linux/qualcommax package/firmware
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware
 
 
 
