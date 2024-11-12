836de081877f83d2204d9af2244c57af1767ee7c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-11-13 05:54:39 +0800

    update

--- devices/qualcommax_ipq50xx/diy.sh
+++ devices/qualcommax_ipq50xx/diy.sh
@@ -5,7 +5,9 @@ shopt -s extglob
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
 rm -rf target/linux/qualcommax package/boot/uboot-envtools package/firmware package/kernel/mac80211 package/kernel/qca-*
-git_clone_path ipq50xx-pr https://github.com/hzyitc/openwrt-redmi-ax3000 target/linux/qualcommax package/firmware package/kernel/mac80211 package/kernel/qca-nss-dp package/kernel/qca-ssdk package/boot/uboot-envtools
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax package/firmware package/kernel/mac80211 package/qca package/boot/uboot-envtools
+
+sed -i "s/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/" target/linux/qualcommax/Makefile
 
 
 
